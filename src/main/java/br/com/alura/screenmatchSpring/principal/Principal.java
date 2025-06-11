b82395645f646afef1de3555ab5216b5afc0facd
CARLOS25052003 <carloscharles016@outlook.com>
2025-06-10 21:02:38 -0400

    Buscando_dados_completos

---
+++ src/main/java/br/com/alura/screenmatchSpring/principal/Principal.java
@@ -0,0 +1,39 @@
+package br.com.alura.screenmatchSpring.principal;
+
+import br.com.alura.screenmatchSpring.Model.DadosSerie;
+import br.com.alura.screenmatchSpring.Model.DadosTemporada;
+import br.com.alura.screenmatchSpring.Service.ConsumoApi;
+import br.com.alura.screenmatchSpring.Service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private Scanner leitura = new Scanner(System.in);
+
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+
+    private final String API_KEY = "&apikey=6fad4aa1";
+
+    public void exibeMenu(){
+        System.out.print("Digite o nome da serie para buscar: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> todasTemporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas() ; i++) {
+            json =consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season="+ i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            todasTemporadas.add(dadosTemporada);
+        }
+        todasTemporadas.forEach(System.out::println);
+    }
+}
